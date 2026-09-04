35e767a3e94fe6687c74f2f2570b3a95af7dff63
AmandaMonteroR <monteroramanda@gmail.com>
2026-09-04 11:14:48 -0600

    Se liga la sesion para eliminar cliente que solo lo puede hacer el admin

--- Publico/api/eliminar_cliente.php
+++ Publico/api/eliminar_cliente.php
@@ -1,9 +1,18 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 require_once __DIR__ . '/../../Aplicacion/Controladoras/ClienteController.php';
+require_once __DIR__ . '/../../Aplicacion/Comun/Sesion.php';
+
+$usuario = Sesion::requerirSesion(Sesion::TIPO_CLIENTE);
 
 $datos = json_decode(file_get_contents('php://input'), true);
 $idCliente = (int) ($datos['idCliente'] ?? 0);
+
+if ($idCliente !== $usuario['id']) {
+    http_response_code(403);
+    echo json_encode(['exito' => false, 'mensaje' => 'No tienes permiso para desactivar esa cuenta']);
+    exit;
+}
 
 try {
     $controlador = new ClienteController();
